96560d77672ff7a6cd83ddc9bf3c29b5ac309125
SaferGo <sfgomez1998@gmail.com>
2020-11-04 01:11:08 +0300

    Creating the main windows

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
-print("Starting the project!")
-print("Testing the new branch.")
+from tkinter import *
+
+my_window = Tk()
+maze_screen = Canvas(my_window, width=500, height=380,background='red')
+menu_screen = Canvas(my_window, width=500, height=120,background='black')
+
+maze_screen.grid(row=0,column=0)
+menu_screen.grid(row=1,column=0)
+
+my_window.mainloop()

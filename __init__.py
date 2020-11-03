8560ba2a30ff0e32d7871f8b80d75a710bb1a45d
SaferGo <sfgomez1998@gmail.com>
2020-11-04 01:36:10 +0300

    generate button

--- __init__.py
+++ __init__.py
@@ -1,10 +1,19 @@
 from tkinter import *
 
 my_window = Tk()
+my_window.title('Maze generator')
 maze_screen = Canvas(my_window, width=500, height=380,background='red')
 menu_screen = Canvas(my_window, width=500, height=120,background='black')
 
 maze_screen.grid(row=0,column=0)
 menu_screen.grid(row=1,column=0)
+
+def draw_maze():
+    maze = generate_maze()
+    print("hi!")
+
+generate_button = Button(my_window, text = 'Generate maze!',command = draw_maze)
+generate_button.grid(row=1,column=0)
+
 
 my_window.mainloop()

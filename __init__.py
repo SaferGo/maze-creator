c08b6cb69c31853a8dfe014fa5ad7e592de0614c
SaferGo <sfgomez1998@gmail.com>
2020-11-05 07:25:54 +0300

    printing just one path on the maze

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ menu_screen = Canvas(my_window, width=500, height=120, background='black')
 maze_screen.pack(fill="both", expand=True)
 menu_screen.pack(fill="both", expand=True)
 
-m = Maze(6)
+m = Maze(20)
 
 
 def create_maze():
@@ -21,34 +21,26 @@ generate_button.pack(side=LEFT)
 
 
 def draw_maze(event):
-    print("----------------------------")
     maze_screen.delete("all")
-    size_step_w = int(event.width / 6)
-    size_step_h = int(event.height / 6)
-    #maze_screen.create_line(5 * size_step_h, 0, 5 * size_step_h, size_step_h)
-    #maze_screen.create_line(0, 5 * size_step_h, size_step_h, 5 * size_step_h)
+    size_step_w = int(event.width / 20)
+    size_step_h = int(event.height / 20)
     # x, y, x y
-    for x in range(6):
-        for y in range(6):
+    for x in range(20):
+        for y in range(20):
             print("--------------------------")
             print("Pos: ", x, y)
             node = m.mz[x][y]
             if node.top_wall:
                 print("Top")
-                maze_screen.create_line(x * size_step_w, y * size_step_h, 2 * x * size_step_w, y * size_step_h)
+                maze_screen.create_line(y * size_step_w, x * size_step_h, (y + 1) * size_step_w, x * size_step_h)
             if node.bottom_wall:
                 print("Bottom")
-                maze_screen.create_line(x * size_step_w, (y + 1) * size_step_h, (x + 1) * size_step_w, (y + 1) * size_step_h)
+                maze_screen.create_line(y * size_step_w, (x + 1) * size_step_h, (y + 1) * size_step_w, (x + 1) * size_step_h)
             if node.right_wall:
                 print("Right!")
-                maze_screen.create_line(x * size_step_w, y * size_step_h, x * size_step_w, (y + 1) * size_step_h)
+                maze_screen.create_line((y + 1) * size_step_w, x * size_step_h, (y + 1) * size_step_w, (x + 1) * size_step_h)
             if node.left_wall:
                 print("Left!")
-
-    # for x in range(size_step_w, event.width, size_step_w):
-    #    maze_screen.create_line(x, 0, x, event.height, fill="#476042")
-    # for x in range(size_step_h, event.height, size_step_h):
-    #    maze_screen.create_line(0, x, event.width, x, fill="#476042")
 
 
 maze_screen.bind("<Configure>", draw_maze)
